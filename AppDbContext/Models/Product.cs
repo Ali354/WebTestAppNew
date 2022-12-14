a8040ab92f45e19b7f824cbe4a841edba6fed06d
Ali354 <mahfoodali795@gmail.com>
2022-12-14 18:00:31 +0200

    Add project files.

---
+++ AppDbContext/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AppDbContext.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Comment = new HashSet<Comment>();
+            ProductAttr = new HashSet<ProductAttr>();
+            ProductOrder = new HashSet<ProductOrder>();
+            Rating = new HashSet<Rating>();
+        }
+
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int Price { get; set; }
+        public int? Dicount { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ICollection<Comment> Comment { get; set; }
+        public virtual ICollection<ProductAttr> ProductAttr { get; set; }
+        public virtual ICollection<ProductOrder> ProductOrder { get; set; }
+        public virtual ICollection<Rating> Rating { get; set; }
+    }
+}
